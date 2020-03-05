3b4db578a1bd2b9295d5fd10e06249d286b81ff2
Edwin S <edwinrs27z@gmail.com>
2020-03-05 12:51:04 -0500

    basic key input for unity
    will likely change to the different funciton in order to restrict press to one time, not a hold down

---
+++ Assets/keyPress.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyPress : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            /*  TO DO:
+             * add C note sound file
+             * when D is pressed it will trigger the sound file once.
+            */
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            /*  TO DO:
+             * add D note sound file
+             * when D is pressed it will trigger the sound file once.
+            */
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            /*  TO DO:
+             * add E note sound file
+             * when E is pressed it will trigger the sound file once.
+            */
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            /*  TO DO:
+             * add F note sound file
+             * when F is pressed it will trigger the sound file once.
+            */
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            /*  TO DO:
+             * add G note sound file
+             * when G is pressed it will trigger the sound file once.
+            */
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            /*  TO DO:
+             * add A note sound file
+             * when A is pressed it will trigger the sound file once.
+            */
+        }
+
+        if (Input.GetKey(KeyCode.B))
+        {
+            /*  TO DO:
+             * add B note sound file
+             * when B is pressed it will trigger the sound file once.
+            */
+        }
+    }
+}
